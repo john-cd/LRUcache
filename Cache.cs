42d72d592c300afbba5fa703fd2a422ce5dd766a
john-cd <sjncd2000@gmail.com>
2017-02-14 13:32:51 -0800

    initial code commit

---
+++ Cache.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CacheLib
+{
+    // LRU cache i.e. a cache that, when low on memory, evicts least recently used items.
+    public class Cache<K, V>
+    {
+        private readonly int _capacity;
+        //
+        private readonly object _syncRoot = new object(); // coarse synchronization for thread safety 
+        internal readonly IDictionary<K, V> _internalStorage; // stores the values
+        internal readonly LinkedList<K> _lru; // stores the most recent used keys; first = most recently used
+
+        public Cache(int capacity = 50_000) 
+        {
+            this._capacity = (capacity > 0)? capacity : throw new ArgumentOutOfRangeException("capacity cannot be less or equal than zero.", nameof(capacity));
+            this._internalStorage = new Dictionary<K, V>(this._capacity);
+            this._lru = new LinkedList<K>();
+        }
+
+        public void Add(K key, V value)
+        {
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
+
+            lock (_syncRoot)
+            {
+                if (!_internalStorage.ContainsKey(key))
+                {
+                    if (_internalStorage.Count >= _capacity)
+                    {
+                        // remove least used key
+                        _internalStorage.Remove(_lru.Last.Value);
+                        _lru.RemoveLast();
+                    }
+                    _internalStorage.Add(key, value);
+                    _lru.Remove(key);
+                    _lru.AddFirst(key);
+                }
+                else
+                    throw new ArgumentException("An element with the same key already exists.", nameof(key));
+            }
+        }
+
+        public bool TryGetValue(K key, out V value)
+        {
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
+            lock (_syncRoot)
+            {
+                if (_internalStorage.TryGetValue(key, out value))
+                {
+                    // key found - move to the top of the list 
+                    _lru.Remove(key);
+                    _lru.AddFirst(key);
+                    return true;
+                }
+                // else - key not found.
+                value = default(V);
+                return false;
+            }
+        }
+
+        // Possible improvements:
+        // * LRU are traditionally implemented using a Hashmap (O(1) access to value) and LinkedList (O(1) access to last). 
+        // For very large datasets, a probabilistic data structure (bloom filter) is a better choice: https://en.wikipedia.org/wiki/Bloom_filter
+        // * Fine-grained concurrency control with read-write locks and/or use of concurent collections.
+        // * Storage in a single data structure is possible, if strict LRU is not required; for example, choose a random subset of elements and evict the oldest.
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using CacheLib;
+using System.Collections.Generic;
+using static System.String;
+
+// Requires C# 7.0.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Example.Instance.Test();
+    }
+}
+
+internal sealed class Example
+{
+    private static readonly Example _instance = new Example();
+
+    static Example()
+    {
+    }
+
+    public static Example Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    // string = address
+    // decimal = sales tax rate
+    private readonly Cache<string, decimal> _cache = new Cache<string, decimal>(3); 
+
+    // this is supposed to contain billions of addresses...
+    private readonly IDictionary<string, decimal> _fakeData = new Dictionary<String, Decimal>();
+    
+    private Example()
+    { 
+    }
+
+    /// <summary>
+    /// Returns the tax rate for a given address.
+    /// </summary>
+    /// <param name="address">The street address.</param>
+    /// <returns>The tax rate.</returns>
+    public decimal Sales_tax_lookup(string address)
+    {
+        if (IsNullOrWhiteSpace(address))
+            throw new ArgumentException("Address cannot be null or whitespace only.", nameof(address));
+        if (_fakeData.TryGetValue(address.Trim(), out decimal taxrate))
+            return taxrate;
+        else
+            throw new ArgumentOutOfRangeException(nameof(address));
+    }
+
+
+    /// <summary>
+    /// Returns the tax rate for a given address (cached version).
+    /// </summary>
+    /// <param name="address">The street address.</param>
+    /// <returns>The tax rate.</returns>
+    public decimal Fast_rate_lookup(string address)
+    {
+        // if cache has the desired address / tax rate, return it; otherwise call the slow function and add value to cache. 
+        if (!_cache.TryGetValue(address, out decimal taxrate))
+        {
+            taxrate = Sales_tax_lookup(address);
+            _cache.Add(address, taxrate);
+            Console.WriteLine("slow call and addition to cache");
+        }
+        return taxrate;
+    }
+
+
+    public void Test()
+    {
+        for (decimal i = 1; i <= 10; i++)
+        {
+            _fakeData.Add($"{i} Main Street Seattle, WA", i / 100m);
+        }
+        foreach (var address in _fakeData)
+        {
+            Console.WriteLine(address);
+        }
+
+        var tests = new string[] { "3 Main Street Seattle, WA", "4 Main Street Seattle, WA", "5 Main Street Seattle, WA", "6 Main Street Seattle, WA", "5 Main Street Seattle, WA", "3 Main Street Seattle, WA"};
+
+        Console.WriteLine("---------- sales_tax_lookup ------------");
+        foreach (string address in tests)
+        {
+            Console.WriteLine("{0} --> {1}", address, Sales_tax_lookup(address));
+        }
+        Console.WriteLine("---------- fast_rate_lookup ------------");
+        foreach (string address in tests)
+        {
+            Console.WriteLine("{0} --> {1}", address, Fast_rate_lookup(address));
+
+            Snoop();
+        }
+        Console.ReadKey(true);
+    }
+
+    private void Snoop()
+    {
+
+        Console.WriteLine(nameof(_cache._internalStorage));
+        foreach (var el in _cache._internalStorage)
+        {
+            Console.WriteLine("{0}: {1}", el.Key, el.Value);
+
+        }
+        Console.WriteLine(nameof(_cache._lru));
+        foreach (var key in _cache._lru)
+        {
+            Console.WriteLine(key);
+        }
+        Console.WriteLine("=====================================");
+    }
+}
